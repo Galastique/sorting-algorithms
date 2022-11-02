5e4134fb0803ba3e2b14310133ab70abf70d021a
Galastique <anthokess@hotmail.com>
2022-11-01 21:59:23 -0400

    added file to clean working directory

--- clean.js
+++ clean.js
@@ -1,6 +1,21 @@
-const { arrays } = require("./logging");
+const fs = require('fs');
 
+//Deletes directories
 function files(){
-    arrays();
-    sorted();
+    cleanArrays();
+    cleanSorted();
 }
+
+function cleanArrays(){
+    if(fs.existsSync("./arrays/")){
+        fs.rmSync("./arrays/", { recursive: true, force: true });
+    }
+}
+
+function cleanSorted(){
+    if(fs.existsSync("./output/")){
+        fs.rmSync("./output/", { recursive: true, force: true });
+    }
+}
+
+module.exports = {files}
